eaecc875a0131a284a60a194640e93af8e13276c
seochan99 <gmlcks0513@naver.com>
2021-04-29 19:57:03 +0900

    thu

--- Unity 2D chans Project/Assets/script/Enemy.cs
+++ Unity 2D chans Project/Assets/script/Enemy.cs
@@ -7,13 +7,14 @@ public class Enemy : MonoBehaviour
 {
     Rigidbody2D rigid;
     public int nextMove; // 행동 지표를 결정할 변수 하나 생성
-
-
-
+    Animator anim;
+    SpriteRenderer spriteRenderer;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
 
         Invoke("Think", 3); //주어진 시간이 지난뒤 지정된 함수를 실행하는 함수 
     }
@@ -29,9 +30,7 @@ public class Enemy : MonoBehaviour
         RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Platform"));
         if (rayHit.collider == null) // 바닥 감지를 위해서 레이저를 쏜다! 없다면!!! 바닥이
         {
-            nextMove = nextMove * -1;
-            CancelInvoke();
-            Invoke("Think", 5);
+            Turn();
         }
     }
 
@@ -39,8 +38,24 @@ public class Enemy : MonoBehaviour
     {
         nextMove = Random.Range(-2, 3);
 
-        Think(); // 딜레이 없이 재귀함수 쓰면 에러 난다!
+
+        //애
+        anim.SetInteger("WalkSpeed", nextMove);
+
+        // 방향
+        if(nextMove != 0)//안서있을때만 방향 바꾸게 설정
+            spriteRenderer.flipX = nextMove == 1; // 1이면 실행
+
+        //재귀함수 
         float nextThinkTime = Random.Range(2f, 5f);
         Invoke("Think", nextThinkTime);
+    }
+    void Turn()
+    {
+        nextMove = nextMove * -1;
+        spriteRenderer.flipX = nextMove == 1; // 1이면 실행
+
+        CancelInvoke();
+        Invoke("Think", 5);
     }
 }
