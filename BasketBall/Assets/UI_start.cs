d8a023581ef439d9ae6e60f0763fe48c1d59da89
seochan99 <gmlcks0513@naver.com>
2021-05-24 01:45:00 +0900

    //

---
+++ BasketBall/Assets/UI_start.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_start : MonoBehaviour
+{
+    void Awake()
+    {
+        Screen.SetResolution(Screen.width, Screen.width * int SetWidth / int SetHeight, bool fullscreen);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
